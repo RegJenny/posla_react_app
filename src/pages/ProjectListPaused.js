19567795fbc781938697bb9d2de6589d723a7c80
RegJenny <103183415+RegJenny@users.noreply.github.com>
2022-06-03 12:02:13 +0100

    Update ProjectListPaused.js

--- src/pages/ProjectListPaused.js
+++ src/pages/ProjectListPaused.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import AccountSidebar from '../components/AccountSidebar';
 import ProjectList from '../components/ProjectList';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import {Link} from 'react-router-dom';
 
 const ProjectListPaused = () =>{
+     const [result, setResult] = useState("");
+    const [error, setError] = useState("");
+
+    useEffect (()=>{
+        fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(response => response.json())
+  .then(response=>{
+    setResult(response)
+  })
+  .catch(error => console.log(error));
+
+    })
+
 	return(
 		<>
 	<Header/>
@@ -53,39 +67,71 @@ const ProjectListPaused = () =>{
                         
                         <div class="project-list project-list-mini mt-20 row">
 
-
+                            {result && result.map((item)=>(
                             <div class="col-sm-6 col-md-12 col-lg-6 project-mgmt">
-                               <ProjectList/>
-                                <div class="dropdown">
-                                    <label class="label label-fff label-sm mr-5 pull-right" style={{marginTop: "7px"}}>
-                                        Paused
-                                    </label>
-                                    <div id="navbarDropdown" class="nav-link bg-blue dropdown-toggle b-1-blue text-fff" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Manage
-                                    </div>
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style={{marginTop: "-20px"}}>
-                                        <a class="dropdown-item" href="/project/c1d00230-a423-4b84-a121-7105239ff8d8678">
-                                            Preview
-                                        </a>
-                                        <a class="dropdown-item cursor-pointer" onClick="event.preventDefault(); document.getElementById('resume-form').submit();">
-                                            Resume
-                                        </a>
-                                        <form id="resume-form" action="" method="POST" class="d-none">
-                                            -- hidden input with project id --
-                                        </form>
-                                        <a class="dropdown-item" href="/account/projects/edit/1234">
-                                            Edit
-                                        </a>
-                                        <a class="dropdown-item" href="/account/projects/delete/1234">
-                                            Delete
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+       
+                                                           <Link to="/project/c1d00230-a423-4b84-a121-7105239ff8d8" className="project" id="projectlist">
+                                    
+                                    <div>
+                                        <div>
+                                            <span className="fa fa-angle-right icon-50"></span>
+                                        </div>
+                                        <div className="">
+                                            <div className="font-bold">
+                                               {item.title}
+                                            </div>
 
+                                            <div className="text-fade ellipsis-2-lines mt-5">
+                                                {item.body} 
+                                                </div>
+                                        </div>
+                                    </div>
 
-                            <div class="col-sm-6 col-md-12 col-lg-6 project-mgmt">
-                               <ProjectList/>
+                                    <div className="mt-5 ellipsis">
+                                        <div className="project-price">
+                                            $150
+                                        </div>
+                                        <div className="item-labels">
+                                            <div className="item-labels-new">
+                                                New
+                                            </div>
+                                            <div className="item-labels-featured">
+                                                Featured
+                                            </div>
+                                            <div className="item-labels-proposals">
+                                                18 proposals
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="item-labels item-labels-tags-all ellipsis">
+                                        <div className="item-labels-prefix">
+                                            Tags & Skills:
+                                        </div>
+                                        <div className="item-labels-tags">
+                                            Tag name
+                                        </div>
+                                        <div className="item-labels-tags">
+                                            Tag name
+                                        </div>
+                                        <div className="item-labels-tags">
+                                            Tag name
+                                        </div>
+                                        <div className="item-labels-tags">
+                                            Tag name
+                                        </div>
+                                        <div className="item-labels-tags">
+                                            Tag name
+                                        </div>
+                                        <div className="item-labels-tags">
+                                            Tag name
+                                        </div>
+                                        <div className="item-labels-tags">
+                                            Tag name
+                                        </div>
+                                    </div>
+
+                                </Link>
                                 <div class="dropdown">
                                     <label class="label label-fff label-sm mr-5 pull-right" style={{marginTop: "7px"}}>
                                         Paused
@@ -113,7 +159,7 @@ const ProjectListPaused = () =>{
                                     </div>
                                 </div>
                             </div>
-
+                            ))}
 
                         </div>
 
